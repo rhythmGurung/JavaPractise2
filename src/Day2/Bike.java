a9565203f33863897b3a2e106db89699d6726ee7
rhythmGurung <gurungrhythm12@gmail.com>
2025-06-18 12:13:11 +0545

    Adding getters and setter

--- src/Day2/Bike.java
+++ src/Day2/Bike.java
@@ -1,19 +1,54 @@
 package Day2;
 public class Bike{
-    String model;
-    String company;
-    String bikeColour;
-    String bikeName;
-    String assembledDate;
-    int engineNumber;
+   private String model;
+   private String company;
+   private String bikeColour;
+   private String bikeName;
+   private String purchasedDate;
+   private int engineNumber;
+   private int velocity;
 
-    Bike(String bikeName ,String model , String company,String bikeColour , String assembledDate , int engineNumber ){
+
+    Bike(String bikeName ,String model , String company,String bikeColour , String purchasedDate , int engineNumber  ){
         this.model = model;
         this.company = company;
         this.bikeColour = bikeColour;
         this.bikeName = bikeName;
-        this.assembledDate = assembledDate;
+        this.purchasedDate = purchasedDate;
         this.engineNumber = engineNumber;
+    }
+
+    public String getModel(){
+        return model;
+    }
+    public String getBikeName(){
+        return bikeName;
+    }
+
+    public String getCompany(){
+        return company;
+    }
+
+    public String getPurchasedDate(){
+        return purchasedDate;
+    }
+
+    public void pressBrake(){
+        System.out.println("Bike is stopped");
+    }
+
+    public  void releaseBrake(){
+        System.out.println("Bike is in motion ");
+    }
+
+    public int accelerate(int velocity){
+
+       System.out.println("Bike is accelerated by  " + velocity  + "meter per second");
+        return velocity;
+    }
+
+    public void turnOnSideLight(){
+        System.out.println("Side light is turned on");
     }
 
 
--- src/Day2/Main.java
+++ src/Day2/Main.java
@@ -1,8 +1,12 @@
 package Day2;
 
 public class Main {
-    public static void main(String[] args){
-        Bike b1 = new Bike("Bullet",);
+    public static void main(String[] args) {
+        Bike b1 = new Bike("Bullet", "Classic350", "Royal Enfield", "Matt Black", "February 14", 12345);
+        b1.pressBrake();
+        b1.releaseBrake();
+        b1.accelerate(100);
+        b1.turnOnSideLight();
 
     }
 }
