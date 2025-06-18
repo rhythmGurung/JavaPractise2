e637b0780af325edc155e1c8ad634811be7a3d9d
rhythmGurung <gurungrhythm12@gmail.com>
2025-06-18 11:47:08 +0545

    added attriubtes and conatructor for creation fo objects

---
+++ src/Day2/Bike.java
@@ -0,0 +1,21 @@
+package Day2;
+public class Bike{
+    String model;
+    String company;
+    String bikeColour;
+    String bikeName;
+    String assembledDate;
+    int engineNumber;
+
+    Bike(String bikeName ,String model , String company,String bikeColour , String assembledDate , int engineNumber ){
+        this.model = model;
+        this.company = company;
+        this.bikeColour = bikeColour;
+        this.bikeName = bikeName;
+        this.assembledDate = assembledDate;
+        this.engineNumber = engineNumber;
+    }
+
+
+
+}
---
+++ src/Day2/Main.java
@@ -0,0 +1,8 @@
+package Day2;
+
+public class Main {
+    public static void main(String[] args){
+        Bike b1 = new Bike("Bullet",);
+
+    }
+}
