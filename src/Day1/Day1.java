c9a2612ceb76a855b6c692a1df84caf916d67860
rhythmGurung <gurungrhythm12@gmail.com>
2025-06-19 09:52:49 +0545

    Using ArrayList to find out the divisors of a number except itself

--- src/Day1/Day1.java
+++ src/Day1/Day1.java
@@ -1,28 +1,20 @@
 package Day1;
+import java.util.ArrayList;
 import java.util.Arrays;
 public class Day1 {
     public static void main(String[] args){
-        int num1 = 12345;
-//Creating a string array and typecasting into string
-        String str = String.valueOf(num1);
-//Splitting array for further typecasting into integer
-        String[] str2 = str.split("");
-//Variable for integer array
-        int[] arrayInt = new int[str.length()];
+        int num = 6 ;
+        ArrayList<Integer> arrayInt = new ArrayList<>();
 
-//Typecasting String array into Integer array
-        for (int i = 0;i<str.length();i++){
-        arrayInt[i] = Integer.parseInt(str2[i]);
-      }
-        System.out.println(Arrays.toString(arrayInt));
-        int j = 0;
-        int temp = 0;
-        while(j< str.length()){
-            temp = temp + arrayInt[j];
-            j++;
-        }
-        System.out.println(temp);
 
+
+       for(int i = 1 ; i < num;i++){
+           if(num % i == 0){
+               arrayInt.add(i);
+           }
+
+       }
+       System.out.println(arrayInt);
 
     }
 }
